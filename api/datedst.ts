e4bd8b8b20f686b592559b1b03e753a62ed0ea50
saadbye <saadbye@gmail.com>
2021-04-07 11:33:33 +0200

    Update datedst.ts

--- api/datedst.ts
+++ api/datedst.ts
@@ -2,6 +2,6 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (_req: VercelRequest, res: VercelResponse) => {
   const date = new Date().toString();
-  const offset = getTimezoneOffset();
+  const offset = new getTimezoneOffset();
   res.status(200).send(date, offset);
 };
